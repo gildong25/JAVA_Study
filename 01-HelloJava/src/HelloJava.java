cc8668ea2a554646f47d87709999a3acebde5af5
Anna <gildong2525@gmail.com>
2019-02-24 19:37:12 +0900

    01-HelloJava

---
+++ 01-HelloJava/src/HelloJava.java
@@ -0,0 +1,25 @@
+/**
+ * 이렇게 두개 누르면 파란색으로 뜨지롱
+ * 프로그램 소스의 최소 단위(=class)를 명시하는 블록.
+ * --> public class 클래스이름
+ * 클래스 이름은 소스 파일의 이름과 동일해야 하며, 영어/숫자 언더바사용가능하다
+ * @author Administrator
+ * 첫 글자는 반드시 영어로만 구성 되어야 한다.
+ */
+public class HelloJava {
+/**
+ * 프로그램의 시작점을 의미하는 블록 -> main 메서드라고 한다.
+ * @param args
+ */
+	public static void main(String[] args) {
+		//콘솔에 문장을 출력하기 위한 명령어
+		//문장을 표현하는 방법 - 쌍따옴표로 묶는다. - 문자열 
+		System.out.println("HelloJava");
+		// ctrl+Shift+F 누르면 알아서 자리정리해줌
+		// TODO Auto-generated method stub
+		// Ctrl+Space 누르면 알아서 함수찾아줌
+	}
+	// 자바친구는 엄격해용~ 대소문자 따옴표 가려주세여~
+	
+
+}
---
+++ 01-HelloJava/src/java/lang/main01.java
@@ -0,0 +1,10 @@
+package java.lang;
+
+public class main01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
