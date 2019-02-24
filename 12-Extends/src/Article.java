1adc0ff0deaf46c8c5b9052b9b583715eccd2fbb
Anna <gildong2525@gmail.com>
2019-02-24 22:45:58 +0900

    12-Extends/클래스확장

---
+++ 12-Extends/src/Article.java
@@ -0,0 +1,16 @@
+
+public class Article {
+   //글번호
+	private int num;
+	//제목
+	private String title;
+	
+	public int getNum() {return num;}
+	
+	public void setNum(int num) {this.num = num;}
+	
+	public String getTitle() {return title;}
+	
+	public void setTitle(String title){ this.title = title;}
+	
+}
---
+++ 12-Extends/src/Snippet.java
@@ -0,0 +1,19 @@
+
+
+public class Snippet {
+	public int times(int x, int y){
+			return x*y;
+		}
+		
+		public int divide(int x, int y){
+			int result = 0;
+			
+			
+			if(y != 0){
+				result = x / y; 
+			}
+			
+			return result;
+		}
+	
+}
