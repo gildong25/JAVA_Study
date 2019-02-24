173a5033d20e56c0ecf9fc16892c8147ec90233f
Anna <gildong2525@gmail.com>
2019-02-24 22:42:20 +0900

    11-Hiding/캡슐화&은닉

---
+++ 11-Hiding/src/Main02.java
@@ -0,0 +1,15 @@
+
+public class Main02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Member s = new Member("JAVA학생", 20);
+		
+		String name = s.getName();
+		System.out.println("이름: " + name);
+		
+		int age = s.getAge();
+		System.out.println("나이: "+age);
+	}
+
+}
