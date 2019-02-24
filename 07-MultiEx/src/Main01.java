e579175ed291a07b50b94be481722e25cd912152
Anna <gildong2525@gmail.com>
2019-02-24 20:45:50 +0900

    07-MultiEx/중첩문

---
+++ 07-MultiEx/src/Main01.java
@@ -0,0 +1,17 @@
+
+public class Main01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int target = 100;
+		
+		if (target == 100){
+			int num = target + 100;
+			System.out.println(num);
+		} else {
+			int num = target - 100;
+			System.out.println(num);
+		}
+	}
+
+}
---
+++ 07-MultiEx/src/Main03.java
@@ -0,0 +1,19 @@
+
+public class Main03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum1 = 0;
+		int sum2 = 0;
+		
+		for (int i = 1; i <= 100; i ++){
+			if (i % 2 == 0) { sum1 += i;}
+			else 			{ sum2 += i;}
+		}
+	System.out.println(sum1);
+	System.out.println(sum2);
+	}
+	
+	
+
+}
