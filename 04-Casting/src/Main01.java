9798282b743bfffcfb4b28c275936948e7b4dde0
Anna <gildong2525@gmail.com>
2019-02-24 20:43:12 +0900

    04-Casting/형변환

---
+++ 04-Casting/src/Main01.java
@@ -0,0 +1,30 @@
+
+public class Main01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		short s = 100;
+		int i = s;
+		long l = i;
+		float f = l;
+		double d = f;
+		System.out.println("s="+s);
+		System.out.println("s="+i);
+		System.out.println("s="+l);
+		System.out.println("s="+f);
+		System.out.println("s="+d);
+		
+		System.out.println("------------");
+		
+		int myage1 = 20;
+		double myage2 = myage1;
+		System.out.println("myage1="+myage1);
+		System.out.println("myage2="+myage2);
+		System.out.println("------------");
+		
+		char c1 = 'a';
+		int num1 = c1;
+		System.out.println("num1="+num1);
+	}
+
+}
---
+++ 04-Casting/src/Main03.java
@@ -0,0 +1,13 @@
+
+public class Main03 {
+	public static void main(String[] args) {
+	double a = 10.5d;
+	float b = 20.5f;
+	
+	float f = (float) (a + b);
+	System.out.println(f);
+	
+	double d = a + b ;
+	System.out.println(d);
+}
+}
