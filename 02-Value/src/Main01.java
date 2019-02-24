6d6e4f80d71423fae786d1d2ce19e9d22f1a2792
Anna <gildong2525@gmail.com>
2019-02-24 20:38:42 +0900

    02-Value/

---
+++ 02-Value/src/Main01.java
@@ -0,0 +1,14 @@
+
+public class Main01 {
+
+	public static void main(String[] args) {
+		int num1;
+		num1 = 100;
+		long num2 = 12345l;
+		double num3 = 128.4d;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+	}
+
+}
---
+++ 02-Value/src/Main02.java
@@ -0,0 +1,16 @@
+
+public class Main02 {
+
+	public static void main(String[] args) {
+		int width = 100;
+		final double PI = 3.14d;
+
+		System.out.println("width = " + width);
+		System.out.println("PI = " + PI);
+		width = 200;
+		System.out.println("width = " + width);
+		System.out.println("PI = " + PI);
+
+	}
+
+}
